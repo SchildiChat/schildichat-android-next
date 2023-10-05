20c25365e2a61cbf8a6eed307983a040c654e2da
SpiritCroc <dev@spiritcroc.de>
2023-10-05 10:33:41 +0200

    Preliminary fastlane store info
    Change-Id: Idcfdad6813383b29b9afa6f93de02b19d85f6912

--- graphics/icon_gen.sh
+++ graphics/icon_gen.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# note: this requires inkscape to be on the path, for svg -> png conversions
+
+# Dependencies:
+# inkscape for svg -> png conversions
+# imagemagick for additional png operations
 
 mydir="$(dirname "$(realpath "$0")")"
 
@@ -38,3 +41,23 @@ file="$mydir/ic_launcher_foreground.svg"
 export_files
 base_folder="$mydir/../app/src/scDebug/res/mipmap"
 export_files
+
+
+# Store icon:
+# - remove adaptive-icon extra-padding by reducing export area by a factor of 2/3
+# - fix size to 512x512
+# - apply background manually with imagemagick
+store_icon="$mydir/../.fastlane/metadata/android/en-US/images/icon.png"
+inkscape "$file" --export-filename="$store_icon.tmp.png" --export-area=36:36:180:180 -w 512 -h 512
+# Read gradient from actual vector drawable
+get_bg_prop() {
+    local prop="$1"
+    cat "$mydir/../app/src/scRelease/res/drawable/ic_launcher_background.xml"|grep "$prop"=|sed 's|.*'"$prop"'=\"\([^\"]*\)".*|\1|'
+}
+bg_angle=`get_bg_prop angle`
+bg_startColor=`get_bg_prop startColor`
+bg_endColor=`get_bg_prop endColor`
+magick -size 512x512 -define gradient:angle=$bg_angle gradient:$bg_startColor-$bg_endColor "$store_icon.bg.png"
+magick composite -gravity center "$store_icon.tmp.png" "$store_icon.bg.png" "$store_icon"
+rm "$store_icon.tmp.png"
+rm "$store_icon.bg.png"

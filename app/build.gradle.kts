c7bdcdab285aba8b3e622592ad4e621aa9276763
SpiritCroc <dev@spiritcroc.de>
2025-01-05 10:15:21 +0100

    Increment version
    Change-Id: I526e3334cf3deb73114a475048ad0348d28bcd42

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
 
     defaultConfig {
         applicationId = "chat.schildi.android"
-        versionCode = 950
-        versionName = "0.7.6.sc19"
+        versionCode = 960
+        versionName = "0.7.6.sc20"
         targetSdk = Versions.TARGET_SDK
 
         // Keep abiFilter for the universalApk
@@ -179,7 +179,7 @@ android {
 }
 
 // SC: downstream package name and versioning, overriding Element default config while reducing merge conflicts
-val scVersion = 19
+val scVersion = 20
 android {
     // Use a flavor for common things that the upstream config will not override by the build type
     flavorDimensions += "package"
@@ -188,8 +188,8 @@ android {
         // Common upstream overrides across all sc variants - only one flavor for this dimension to ensure it's picked up!
         create("sc") {
             dimension = "package"
-            versionCode = 950
-            versionName = "0.7.6.sc19"
+            versionCode = 960
+            versionName = "0.7.6.sc20"
             isDefault = true
         }
         // SC variants for different release tracks. Cannot do actual release types for those since fdroid build tools always want `release` builds.

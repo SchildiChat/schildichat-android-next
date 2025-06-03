3278ab34d386f7ae3fa16451442baa0a02bef08f
SpiritCroc <dev@spiritcroc.de>
2025-06-03 19:30:35 +0200

    Increment version to 0.10.2
    Change-Id: If6aeec1a6108b9319e31f2bba14ba110714f95e4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,8 @@ android {
     defaultConfig {
         //applicationId = BuildTimeConfig.APPLICATION_ID
         applicationId = "chat.schildi.android"
-        versionCode = 1010
-        versionName = "0.10.1-ex_25_5_3"
+        versionCode = 1020
+        versionName = "0.10.2-ex_25_5_4"
         targetSdk = Versions.TARGET_SDK
 
         // Keep abiFilter for the universalApk
@@ -203,7 +203,7 @@ android {
 // SC: downstream package name and versioning, overriding Element default config while reducing merge conflicts
 val scVersionMajor = 0
 val scVersionMinor = 10
-val scVersionPatch = 1
+val scVersionPatch = 2
 // Following val is set by increment_version.sh based on the values above
 val scVersionMain = "0.7.6"
 android {
@@ -214,8 +214,8 @@ android {
         // Common upstream overrides across all sc variants - only one flavor for this dimension to ensure it's picked up!
         create("sc") {
             dimension = "package"
-            versionCode = 1010
-            versionName = "0.10.1-ex_25_5_3"
+            versionCode = 1020
+            versionName = "0.10.2-ex_25_5_4"
             isDefault = true
         }
         // SC variants for different release tracks. Cannot do actual release types for those since fdroid build tools always want `release` builds.

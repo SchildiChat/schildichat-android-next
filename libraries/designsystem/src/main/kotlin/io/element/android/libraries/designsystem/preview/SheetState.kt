28db80aed26f0d5f3eff23ff338520cee56861b1
Benoit Marty <benoit@matrix.org>
2025-09-29 17:49:03 +0200

    Fix compilation errors and warnings.

--- libraries/designsystem/src/main/kotlin/io/element/android/libraries/designsystem/preview/SheetState.kt
+++ libraries/designsystem/src/main/kotlin/io/element/android/libraries/designsystem/preview/SheetState.kt
@@ -12,12 +12,12 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.SheetValue
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalDensity
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun sheetStateForPreview() = SheetState(
     skipPartiallyExpanded = true,
+    positionalThreshold = { 0.5f },
+    velocityThreshold = { 400f },
     initialValue = SheetValue.Expanded,
-    density = LocalDensity.current,
 )

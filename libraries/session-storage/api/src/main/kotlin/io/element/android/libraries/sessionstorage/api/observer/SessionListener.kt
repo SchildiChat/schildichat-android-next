752a4ea8462fb994ea315851e05657e865717de1
Benoit Marty <benoit@matrix.org>
2025-10-23 18:03:22 +0200

    Fix detekt issue.

--- libraries/session-storage/api/src/main/kotlin/io/element/android/libraries/sessionstorage/api/observer/SessionListener.kt
+++ libraries/session-storage/api/src/main/kotlin/io/element/android/libraries/sessionstorage/api/observer/SessionListener.kt
@@ -8,6 +8,6 @@
 package io.element.android.libraries.sessionstorage.api.observer
 
 interface SessionListener {
-    suspend fun onSessionCreated(userId: String) = Unit
-    suspend fun onSessionDeleted(userId: String, wasLastSession: Boolean) = Unit
+    suspend fun onSessionCreated(userId: String) {}
+    suspend fun onSessionDeleted(userId: String, wasLastSession: Boolean) {}
 }

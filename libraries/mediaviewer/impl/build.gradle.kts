e6c72df67fc94a5688ee47ea0ac015f45ed29e8f
Benoit Marty <benoit@matrix.org>
2025-10-28 18:40:09 +0100

    Remove unused dependency

--- libraries/mediaviewer/impl/build.gradle.kts
+++ libraries/mediaviewer/impl/build.gradle.kts
@@ -33,7 +33,6 @@ dependencies {
     implementation(libs.telephoto.flick)
 
     implementation(projects.features.enterprise.api)
-    implementation(projects.features.forward.api)
     implementation(projects.features.viewfolder.api)
     implementation(projects.libraries.androidutils)
     implementation(projects.libraries.architecture)

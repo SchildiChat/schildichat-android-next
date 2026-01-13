6d1ed5967bd7a11211e6cde5691bc5eb30f3ff22
Jorge Martin Espinosa <jorgem@element.io>
2026-01-13 14:35:49 +0100

    Create spaces (#5982)
    * Allow creating a space with `CreateRoomParameters`
    
    * Add 'Create space' menu item in the spaces home screen. Also, imports new strings related to spaces.
    
    * Link the 'Create space' button with the screen to create the space
    
    * Unify room access and visibility for `ConfigureRoom`, use the updated design
    
    * Fix `EditRoomDetails` avatar size (68dp)
    
    * Replace `EditableAvatarView` and `UnsavedAvatar` copmonents with `AvatarPickerView`
    
    * `AvatarDataFetcherFactory`: Make sure we use a fallback image fetcher when the URL is not an MXC one (a local one, i.e.). This removes the previous need for a separate `UnsavedAvatarView`
    
    * Use `AvatarPickerView` in all the screens where `EditableAvatarView` was used
    
    * Improve naming and previews
    
    * Update strings, remove unused ones for `RoomAccessItem`
    
    * Make `isSpace` part of the `CreateRoomConfig`
    
    * Ensure the content fits in the screenshots for `AvatarPickerSizesPreview`
    
    * Add `AvatarDataFetcherFactoryTest`
    
    * Add new feature flag for creating spaces
    
    * Fix ripple being too large for the `Pick` state
    
    * Tweak margins and section titles a bit
    
    * Add preview for `HomeTopBar` with the spaces case
    
    * Update screenshots
    
    ---------
    
    Co-authored-by: ElementBot <android@element.io>

--- features/createroom/impl/src/main/kotlin/io/element/android/features/createroom/impl/configureroom/ConfigureRoomPresenter.kt
+++ features/createroom/impl/src/main/kotlin/io/element/android/features/createroom/impl/configureroom/ConfigureRoomPresenter.kt
@@ -19,7 +19,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.core.net.toUri
-import dev.zacsweers.metro.Inject
+import dev.zacsweers.metro.Assisted
+import dev.zacsweers.metro.AssistedFactory
+import dev.zacsweers.metro.AssistedInject
 import im.vector.app.features.analytics.plan.CreatedRoom
 import io.element.android.libraries.architecture.AsyncAction
 import io.element.android.libraries.architecture.Presenter
@@ -49,8 +51,9 @@ import kotlinx.coroutines.launch
 import timber.log.Timber
 import kotlin.jvm.optionals.getOrDefault
 
-@Inject
+@AssistedInject
 class ConfigureRoomPresenter(
+    @Assisted private val isSpace: Boolean,
     private val dataStore: CreateRoomConfigStore,
     private val matrixClient: MatrixClient,
     private val mediaPickerProvider: PickerProvider,
@@ -61,13 +64,22 @@ class ConfigureRoomPresenter(
     private val roomAliasHelper: RoomAliasHelper,
     private val mediaOptimizationConfigProvider: MediaOptimizationConfigProvider,
 ) : Presenter<ConfigureRoomState> {
+    @AssistedFactory
+    interface Factory {
+        fun create(isSpace: Boolean): ConfigureRoomPresenter
+    }
+
     private val cameraPermissionPresenter: PermissionsPresenter = permissionsPresenterFactory.create(android.Manifest.permission.CAMERA)
     private var pendingPermissionRequest = false
+
+    init {
+        dataStore.setIsSpace(isSpace)
+    }
 
     @Composable
     override fun present(): ConfigureRoomState {
         val cameraPermissionState = cameraPermissionPresenter.present()
-        val createRoomConfig by dataStore.getCreateRoomConfigFlow().collectAsState(CreateRoomConfig())
+        val createRoomConfig by dataStore.getCreateRoomConfigFlow().collectAsState()
         val homeserverName = remember { matrixClient.userIdServerName() }
         val isKnockFeatureEnabled by remember {
             featureFlagService.isFeatureEnabledFlow(FeatureFlags.Knock)
@@ -171,7 +183,8 @@ class ConfigureRoomPresenter(
                     preset = RoomPreset.PUBLIC_CHAT,
                     invite = config.invites.map { it.userId },
                     avatar = avatarUrl,
-                    roomAliasName = config.roomVisibility.roomAddress()
+                    roomAliasName = config.roomVisibility.roomAddress(),
+                    isSpace = isSpace,
                 )
             } else {
                 CreateRoomParameters(
@@ -184,6 +197,7 @@ class ConfigureRoomPresenter(
                     preset = RoomPreset.PRIVATE_CHAT,
                     invite = config.invites.map { it.userId },
                     avatar = avatarUrl,
+                    isSpace = isSpace,
                 )
             }
             matrixClient.createRoom(params)

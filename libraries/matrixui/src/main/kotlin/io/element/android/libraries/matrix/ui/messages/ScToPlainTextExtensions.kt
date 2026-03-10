3f2831493367bd5df4497ccab7afd5a964c1ebc4
SpiritCroc <dev@spiritcroc.de>
2026-03-10 19:46:58 +0100

    Better placeholder for spoilers in inbox preview
    Change-Id: I82fce80d44496f343e65aea3fed3b4810c0aa845

--- libraries/matrixui/src/main/kotlin/io/element/android/libraries/matrix/ui/messages/ScToPlainTextExtensions.kt
+++ libraries/matrixui/src/main/kotlin/io/element/android/libraries/matrix/ui/messages/ScToPlainTextExtensions.kt
@@ -5,7 +5,7 @@ import org.jsoup.nodes.TextNode
 
 fun Document.stripSpoilers(): Document {
     select("span[data-mx-spoiler]").forEach { span ->
-        span.replaceWith(TextNode("■".repeat(span.text().length.coerceIn(0, 5))))
+        span.replaceWith(TextNode("█".repeat(span.text().length.coerceIn(0, 5))))
     }
     return this
 }
